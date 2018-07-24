2fe4d6f460c2ee6cd4e543c1e949c940a74e1bbc
pmelse <pmelse@uwaterloo.ca>
2018-07-24 13:16:21 -0400

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -97,6 +97,7 @@ class mailman (
   $virtual_host_overview = false,
   $smtp_max_rcpts        = '500',
   $cgi_dir               = $mailman::params::cgi_dir,
+  $icons_dir             = $mailman::params::icons_dir,
   $list_data_dir         = $mailman::params::list_data_dir,
   $log_dir               = $mailman::params::log_dir,
   $lock_dir              = $mailman::params::lock_dir,
